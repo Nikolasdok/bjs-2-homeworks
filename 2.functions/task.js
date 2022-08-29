ae49c37a4889df84c787d04d2a4eaf349108e69b
Николай Докучаев <pistermanama@gmail.com>
2022-08-29 23:37:41 +0700

    tasks_1,2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,54 @@
-// Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
 
-  // Ваш код
+"use strict"
 
-  return { min: min, max: max, avg: avg };
+function quadraticEquation(a, b, c) {
+    if(a == 0)
+        return false;
+    
+    let D = b * b - 4 * a * c;
+    console.log('D = ' + D);
+    if(D < 0) {
+        return false;
+    res['discriminant'] = D;
+    } else if(D == 0) {
+        res['quadratic roots'] = (-b + Math.sqrt(D)) / (2 * a);
+    } else if(D > 0){
+        let tmp = [];
+        tmp.push((-b + Math.sqrt(D)) / (2 * a));
+        tmp.push((-b - Math.sqrt(D)) / (2 * a));
+        res['quadratic roots'] = tmp;
+    }
+    return res;
 }
+let res = {};
+quadraticEquation(4, 9, 5);
+console.log(res);
 
-// Задание 2
-function worker(arr) {
-  let sum;
 
-  // Ваш код
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
 
-  return sum;
-}
 
-function makeWork(arrOfArr, func) {
-  let max;
+  "use strict";
 
-  // Ваш кода
-  // for ...
+  let now = new Date();
   
-  return max;
-}
+  if (isNaN(percent) || percent < 0) {
+    return(`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
+  } else if (isNaN(contribution)) {
+    return(`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
+  } else if (isNaN(amount)) {
+    return(`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
+  } else if (now > date) {
+    return(`Параметр "Срок ипотеки" содержит неправильное значение "${date}"`);
+  } else {
+    let n = (date.getFullYear() - now.getFullYear()) * 12 - date.getMonth() + now.getMonth();
+    let s = amount - contribution;
+    let p = percent / 1200;
+    let payment = s * (p + (p / (((1 + p)**n) - 1)));
+    totalAmount = Math.round(n * payment*100)/100;
+  }
+  
 
-// Задание 3
-function worker2(arr) {
-  // Ваш код
+  return totalAmount;
 }

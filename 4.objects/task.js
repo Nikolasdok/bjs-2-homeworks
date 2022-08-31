deb45acb1540caf98f91467d7dcf6c1f20636246
Николай Докучаев <pistermanama@gmail.com>
2022-08-31 14:04:11 +0700

    objects-decision-1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,49 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+// Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+  let student3 = new Student ("Vasy", "flame", "20");
+  let student4 = new Student ("vasiliya", "male", "21");
+  let alex = new Student('Alex', 'male', 34);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+    this.subject = subjectName;
+  }
 
-// ваш код для остальных методов
+  Student.prototype.addMark = function (mark) {
+    if(this.marks === undefined){ 
+      this.marks = [mark]; 
+    } else {
+      this.marks.push(mark);
+    }
+  }
+
+
+  Student.prototype.addMarks = function (...mark) {
+    if (this.marks === undefined) {
+      this.marks = mark;
+    } else {
+      this.marks.push(...mark);
+    }
+  }
+
+  alex.addMarks(1,2,3);
+  alex.addMarks(4,5,6);
+  
+  Student.prototype.getAverage = function () {
+    return this.marks.reduce((a, b) => a + b, 0) / this.marks.length;;
+  }
+alex.getAverage();
+
+  
+
+  
+  Student.prototype.exclude = function (reason) {
+    delete this.marks;
+    delete this.subject;
+  
+    this.excluded = reason;
+  }

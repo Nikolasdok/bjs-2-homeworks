23c754129b9f5455a313cbd00a11c187603994e7
Николай Докучаев <pistermanama@gmail.com>
2022-08-31 11:15:01 +0700

    functions-decision-1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,54 +1,58 @@
 
-"use strict"
-
-function quadraticEquation(a, b, c) {
-    if(a == 0)
-        return false;
-    
-    let D = b * b - 4 * a * c;
-    console.log('D = ' + D);
-    if(D < 0) {
-        return false;
-    res['discriminant'] = D;
-    } else if(D == 0) {
-        res['quadratic roots'] = (-b + Math.sqrt(D)) / (2 * a);
-    } else if(D > 0){
-        let tmp = [];
-        tmp.push((-b + Math.sqrt(D)) / (2 * a));
-        tmp.push((-b - Math.sqrt(D)) / (2 * a));
-        res['quadratic roots'] = tmp;
+// Задание 1
+function getArrayParams(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0; 
+  let avg = 0;
+  for (let i=0; i<arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
     }
-    return res;
-}
-let res = {};
-quadraticEquation(4, 9, 5);
-console.log(res);
-
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-
-  "use strict";
-
-  let now = new Date();
-  
-  if (isNaN(percent) || percent < 0) {
-    return(`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
-  } else if (isNaN(contribution)) {
-    return(`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
-  } else if (isNaN(amount)) {
-    return(`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
-  } else if (now > date) {
-    return(`Параметр "Срок ипотеки" содержит неправильное значение "${date}"`);
-  } else {
-    let n = (date.getFullYear() - now.getFullYear()) * 12 - date.getMonth() + now.getMonth();
-    let s = amount - contribution;
-    let p = percent / 1200;
-    let payment = s * (p + (p / (((1 + p)**n) - 1)));
-    totalAmount = Math.round(n * payment*100)/100;
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    sum = sum + arr[i];
   }
-  
+  avg = sum / arr.length;
+  avg = avg.toFixed(2);
+  avg = Number(avg);
+  return { min: min, max: max, avg: avg };
+}
 
-  return totalAmount;
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+  for (let i=0; i<arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
+  for (let i=0; i<arrOfArr.length; i++) {
+    let result = func(arrOfArr[i]);
+    if (result > max) {
+      max = result;
+    }
+  }
+  return max;
+}
+
+// Задание 3
+function worker2(arr) {
+  let minimum = arr[0];
+  let maximum = arr[0];
+  let difference;
+  for (let i=0; i<arr.length; i++) {
+    if (minimum > arr[i]) {
+      minimum = arr[i];
+    }
+    if (maximum < arr[i]) {
+      maximum = arr[i];
+    }
+  }
+  difference = (minimum - maximum);
+  difference = Math.abs(difference);
+  return difference;
 }

6ae41239b61a47f6bc86fac22db318df7fd3a91f
Николай Докучаев <pistermanama@gmail.com>
2022-08-31 14:06:00 +0700

    objects-decision-2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,5 +1,4 @@
 function Student(name, gender, age) {
-// Ваш код
     this.name = name;
     this.gender = gender;
     this.age = age;
@@ -7,12 +6,9 @@ function Student(name, gender, age) {
   let student3 = new Student ("Vasy", "flame", "20");
   let student4 = new Student ("vasiliya", "male", "21");
   let alex = new Student('Alex', 'male', 34);
-
-
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
   }
-
   Student.prototype.addMark = function (mark) {
     if(this.marks === undefined){ 
       this.marks = [mark]; 
@@ -20,8 +16,6 @@ Student.prototype.setSubject = function (subjectName) {
       this.marks.push(mark);
     }
   }
-
-
   Student.prototype.addMarks = function (...mark) {
     if (this.marks === undefined) {
       this.marks = mark;
@@ -29,18 +23,12 @@ Student.prototype.setSubject = function (subjectName) {
       this.marks.push(...mark);
     }
   }
-
   alex.addMarks(1,2,3);
   alex.addMarks(4,5,6);
-  
   Student.prototype.getAverage = function () {
     return this.marks.reduce((a, b) => a + b, 0) / this.marks.length;;
   }
 alex.getAverage();
-
-  
-
-  
   Student.prototype.exclude = function (reason) {
     delete this.marks;
     delete this.subject;
